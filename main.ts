f9a266c6a135d1fb10531d47e2757c2b3e2b5cce
ChabeGuy <114693918+ChabeGuy@users.noreply.github.com>
2022-10-04 11:03:27 -0600

    Changed a few things.

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-    strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.White))
-    if (playerPos < strip.length() - 1) {
-        playerPos += 1
-    }
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    shotRange = strip.range(playerPos - 2, 5)
+    shotRange.showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.pause(250)
+    shotRange.showColor(neopixel.colors(NeoPixelColors.White))
     strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Blue))
     strip.show()
 })
@@ -14,9 +17,10 @@ input.onButtonPressed(Button.B, function () {
     strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Blue))
     strip.show()
 })
+let shotRange: neopixel.Strip = null
 let playerPos = 0
 let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P0, 60, NeoPixelMode.RGB)
 playerPos = 0
 strip.clear()
 strip.showColor(neopixel.colors(NeoPixelColors.White))
@@ -25,4 +29,14 @@ strip.show()
 strip.setBrightness(100)
 basic.forever(function () {
 	
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.White))
+        if (playerPos < strip.length() - 1) {
+            playerPos += 1
+        }
+        strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
 })
