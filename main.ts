bb9ea5a52aaafb4cbcbbc2b790848e7260034993
ChabeGuy <114693918+ChabeGuy@users.noreply.github.com>
2022-10-04 11:22:17 -0600

    Changed controls a little. Started the score system.

--- main.ts
+++ main.ts
@@ -1,19 +1,8 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
 input.onButtonPressed(Button.AB, function () {
     shotRange = strip.range(playerPos - 2, 5)
     shotRange.showColor(neopixel.colors(NeoPixelColors.Blue))
     basic.pause(250)
     shotRange.showColor(neopixel.colors(NeoPixelColors.White))
-    strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Blue))
-    strip.show()
-})
-input.onButtonPressed(Button.B, function () {
-    strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.White))
-    if (playerPos > 0) {
-        playerPos += -1
-    }
     strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Blue))
     strip.show()
 })
@@ -28,9 +17,6 @@ strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
 strip.show()
 strip.setBrightness(100)
 basic.forever(function () {
-	
-})
-basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.White))
         if (playerPos < strip.length() - 1) {
@@ -38,5 +24,18 @@ basic.forever(function () {
         }
         strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Blue))
         strip.show()
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.White))
+        if (playerPos > 0) {
+            playerPos += -1
+        }
+        strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
+})
+basic.forever(function () {
+    if (playerPos >= strip.length() - 1) {
+        game.addScore(1)
     }
 })
