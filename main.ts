aa88f3c6d5785ea303d5965ba1bf6cce3050dcdd
ChabeGuy <114693918+ChabeGuy@users.noreply.github.com>
2022-10-04 19:09:15 -0600

    Added functional enemies. Changed a few things with player behavior.

--- main.ts
+++ main.ts
@@ -1,26 +1,47 @@
 input.onButtonPressed(Button.AB, function () {
     if (inputAvailable == true) {
         inputAvailable = false
-        shotRange = strip.range(playerPos - 2, 5)
+        shotRange = strip.range(playerPos, 3)
+        shotPos = playerPos + 3
         shotRange.showColor(neopixel.colors(NeoPixelColors.Red))
         basic.pause(250)
-        shotRange.showColor(neopixel.colors(NeoPixelColors.White))
+        shotRange.showColor(neopixel.colors(NeoPixelColors.Black))
         strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Red))
         strip.show()
+        for (let enemy of unmovedEnemies) {
+            if (enemy < shotPos && enemy > playerPos) {
+                unmovedEnemies.removeAt(unmovedEnemies.indexOf(enemy))
+            }
+        }
         inputAvailable = true
     }
 })
+let shotPos = 0
 let shotRange: neopixel.Strip = null
+let unmovedEnemies: number[] = []
 let playerPos = 0
 let strip: neopixel.Strip = null
 let inputAvailable = false
 inputAvailable = false
 strip = neopixel.create(DigitalPin.P0, 60, NeoPixelMode.RGB)
 playerPos = 0
+unmovedEnemies = []
+let enemySpawnRate = 4000
+let gameOver = false
 strip.clear()
 strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
 strip.show()
 inputAvailable = true
+loops.everyInterval(500, function () {
+    if (gameOver == false) {
+        for (let enemy of unmovedEnemies) {
+            strip.setPixelColor(enemy, neopixel.colors(NeoPixelColors.Black))
+            strip.setPixelColor(enemy - 1, neopixel.colors(NeoPixelColors.Violet))
+            strip.show()
+            unmovedEnemies[unmovedEnemies.indexOf(enemy)] = enemy - 1
+        }
+    }
+})
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         if (inputAvailable == true) {
@@ -62,5 +83,19 @@ basic.forever(function () {
         strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Red))
         strip.show()
         inputAvailable = true
+    }
+    if (playerPos == unmovedEnemies[unmovedEnemies.indexOf(playerPos)]) {
+        gameOver = true
+        inputAvailable = false
+        strip.clear()
+        strip.show()
+        game.gameOver()
+    }
+})
+loops.everyInterval(enemySpawnRate, function () {
+    if (gameOver == false) {
+        unmovedEnemies.push(59)
+        strip.setPixelColor(unmovedEnemies[unmovedEnemies.length - 1], neopixel.colors(NeoPixelColors.Violet))
+        strip.show()
     }
 })
