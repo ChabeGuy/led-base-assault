54017f34524e360187ce3f58ddb5b1cbe39f7275
ChabeGuy <114693918+ChabeGuy@users.noreply.github.com>
2022-10-05 20:12:33 -0600

    Changed to be controlled by radio. 0: Reset, 1: Left, 2: Right, 3: Shoot, 4: Start.

--- main.ts
+++ main.ts
@@ -1,41 +1,58 @@
-input.onButtonPressed(Button.AB, function () {
-    if (inputAvailable == true) {
-        inputAvailable = false
-        shotRange = strip.range(playerPos, 3)
-        shotPos = playerPos + 3
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 1589, 1, 255, 0, 317, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
-        shotRange.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(250)
-        shotRange.showColor(neopixel.colors(NeoPixelColors.Black))
-        for (let enemy of unmovedEnemies) {
-            if (enemy < shotPos && enemy > playerPos) {
-                unmovedEnemies.removeAt(unmovedEnemies.indexOf(enemy))
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        moveRight = false
+        moveLeft = false
+    } else if (receivedNumber == 1) {
+        moveLeft = true
+    } else if (receivedNumber == 2) {
+        moveRight = true
+    } else if (receivedNumber == 3) {
+        if (inputAvailable == true) {
+            inputAvailable = false
+            shotRange = strip.range(playerPos, 3)
+            shotPos = playerPos + 3
+            music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 1589, 1, 255, 0, 317, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+            shotRange.showColor(neopixel.colors(NeoPixelColors.Red))
+            basic.pause(250)
+            shotRange.showColor(neopixel.colors(NeoPixelColors.Black))
+            for (let enemy of unmovedEnemies) {
+                if (enemy < shotPos && enemy > playerPos) {
+                    unmovedEnemies.removeAt(unmovedEnemies.indexOf(enemy))
+                }
+            }
+            if (gameOver == false) {
+                strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Red))
+                strip.show()
+                inputAvailable = true
             }
         }
-        if (gameOver == false) {
-            strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Red))
-            strip.show()
+    } else if (receivedNumber == 4) {
+        if (ableToStart == true) {
+            ableToStart = false
+            gameOver = false
+            music.playSoundEffect(music.builtinSoundEffect(soundExpression.spring), SoundExpressionPlayMode.InBackground)
             inputAvailable = true
+            game.setScore(0)
         }
     }
 })
+input.onButtonPressed(Button.AB, function () {
+	
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (ableToStart == true) {
-        ableToStart = false
-        gameOver = false
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.spring), SoundExpressionPlayMode.InBackground)
-        inputAvailable = true
-        game.setScore(0)
-    }
+	
 })
 let shotPos = 0
 let shotRange: neopixel.Strip = null
+let moveLeft = false
+let moveRight = false
 let ableToStart = false
 let gameOver = false
 let unmovedEnemies: number[] = []
 let playerPos = 0
 let strip: neopixel.Strip = null
 let inputAvailable = false
+radio.setGroup(1)
 inputAvailable = false
 strip = neopixel.create(DigitalPin.P0, 60, NeoPixelMode.RGB)
 playerPos = 0
@@ -74,7 +91,7 @@ loops.everyInterval(500, function () {
     }
 })
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
+    if (moveRight == true) {
         if (inputAvailable == true) {
             strip.setBrightness(50)
             strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.White))
@@ -86,7 +103,7 @@ basic.forever(function () {
             basic.pause(25)
         }
     }
-    if (input.buttonIsPressed(Button.B)) {
+    if (moveLeft == true) {
         if (inputAvailable == true) {
             strip.setBrightness(50)
             strip.setPixelColor(playerPos, neopixel.colors(NeoPixelColors.Black))
@@ -128,6 +145,7 @@ basic.forever(function () {
         strip.clear()
         strip.show()
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.twinkle), SoundExpressionPlayMode.InBackground)
+        radio.sendNumber(game.score())
         game.gameOver()
     }
 })
